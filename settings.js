6413a45da910ec9daba05a487526d1662b7c5582
sunethm <107301786+sunethm@users.noreply.github.com>
2022-06-12 16:52:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['940770378874']
+8global.ownernomer = '940770378874'
+global.ownername = '🐦suneth karunarathna'
+global.botname = 'sakkara'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
